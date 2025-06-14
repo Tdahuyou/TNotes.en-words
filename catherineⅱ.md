44da2600c9726a03fa7f05eaacc43ed65371a6f1
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-06-15 00:29:56 +0800

    update

---
+++ catherineⅱ.md
@@ -0,0 +1,9 @@
+- CatherineⅡ
+  - 发音
+    - 英 /'kæθrɪn/
+    - 美 /'kæθərin/
+  - 词义
+  - 短语
+    - catherine zeta-jones 凯瑟琳·泽塔琼斯（英国演员）
+    - catherine of aragon 凯萨琳（英国国王亨利八世的第一个王后）
+  - 补充
---
+++ scripts/to_lower_case.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+// 获取当前目录下的所有文件和文件夹
+const dirs = fs.readdirSync(__dirname, '../')
+
+// 遍历目录中的每一个文件/文件夹
+dirs.forEach((file) => {
+  // 如果是 .md 文件
+  if (path.extname(file) === '.md') {
+    // 构建原始路径和新路径
+    const oldPath = path.join(__dirname, '../', file)
+    const newPath = path.join(__dirname, '../', file.toLowerCase())
+
+    // 如果新旧路径不同，则进行重命名
+    if (oldPath !== newPath) {
+      fs.renameSync(oldPath, newPath)
+      console.log(`Renamed: ${oldPath} -> ${newPath}`)
+    }
+  }
+})
