b6103a204473ea6a64399a34380977e8ddfb6d11
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-06-17 22:11:47 +0800

    update

--- scripts/2. 将词库中的所有单词转为小写形式/to_lower_case.js
+++ scripts/2. 将词库中的所有单词转为小写形式/to_lower_case.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 // 获取当前目录下的所有文件和文件夹
-const dirs = fs.readdirSync(path.join(__dirname, '..'))
+const dirs = fs.readdirSync(path.join(__dirname, '..', '..'))
 
 // console.log(dirs)
 
@@ -11,8 +11,8 @@ dirs.forEach((file) => {
   // 如果是 .md 文件
   if (path.extname(file) === '.md') {
     // 构建原始路径和新路径
-    const oldPath = path.join(__dirname, '../', file)
-    const newPath = path.join(__dirname, '../', file.toLowerCase())
+    const oldPath = path.join(__dirname, '../', '../', file)
+    const newPath = path.join(__dirname, '../', '../', file.toLowerCase())
 
     // 如果新旧路径不同，则进行重命名
     if (oldPath !== newPath) {
